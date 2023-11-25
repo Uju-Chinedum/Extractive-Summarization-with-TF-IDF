710950f4718d5d6bf009377a125357f125082016
Savadow <ujuchimaraoke@gmail.com>
2023-11-25 12:35:29 +0100

    feat: addd abstractive text summarization

--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
 # Imports
 from flask import Flask, request, jsonify
-from summarization import summarize_text
+from summarization import extractive_summarization, abstractive_summarization
 import os
 
 # Variable Declaration
@@ -12,9 +12,16 @@ app = Flask(__name__)
 @app.route("/summarize", methods=["POST"])
 def run_summarization():
     data = request.json
+    
     text = data["text"]
-    summary = summarize_text(text)
-    return jsonify({"summary": summary})
+    sentences = data["sentences"]
+    max_length = data["max_length"]
+    min_length = data["min_length"]
+    
+    extractive = extractive_summarization(input_text=text, sentence_length=sentences)
+    abstractive = abstractive_summarization(input_text=text, max_length=max_length, min_length=min_length)
+    
+    return jsonify({"extractive": extractive, "abstractive": abstractive})
 
 
 # Body
--- server/summarization.py
+++ server/summarization.py
@@ -3,13 +3,16 @@ import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 # Download NLTK data for tokenization and stopwords
 nltk.download('punkt')
 nltk.download('stopwords')
 
 # Function Definition
-def summarize_text(input_text, summary_length=7):
+
+
+def extractive_summarization(input_text, sentence_length=7):
     # Tokenize the text into sentences
     sentences = sent_tokenize(input_text)
 
@@ -33,7 +36,7 @@ def summarize_text(input_text, summary_length=7):
 
     # Get the top N sentences with the highest TF-IDF scores as the summary
     summary_indices = sentence_scores.argsort(
-        axis=0)[-summary_length:][::-1].flatten().tolist()[0]
+        axis=0)[-sentence_length:][::-1].flatten().tolist()[0]
     summary_sentences = [sentences[i] for i in summary_indices]
 
     # Join the summary sentences to create the final summary
@@ -41,8 +44,29 @@ def summarize_text(input_text, summary_length=7):
 
     return summary
 
+
+def abstractive_summarization(input_text, max_length=150, min_length=50):
+    # Load pre-trained T5 model and tokenizer
+    model = T5ForConditionalGeneration.from_pretrained("t5-small")
+    tokenizer = T5Tokenizer.from_pretrained("t5-small")
+
+    # Tokenize and format the input text
+    input_ids = tokenizer.encode(
+        "summarize: " + input_text, return_tensors="pt", max_length=512, truncation=True)
+
+    # Generate summary using the T5 model
+    summary_ids = model.generate(input_ids, max_length=max_length, min_length=min_length,
+                                 length_penalty=2.0, num_beams=4, early_stopping=True)
+
+    # Decode the generated summary
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+    return summary
+
+
 # Body
 if __name__ == "__main__":
     text = input()
-    summary = summarize_text(text)
-    print(summary)
+    extractive = extractive_summarization(text)
+    abstractive = abstractive_summarization(text)
+    print({"extractive": extractive, "abstractive": abstractive})
