2f7672b301f9403e71d69ec1d43bd6a0a41566db
Kalu Daniel <krayzhee419@gmail.com>
2024-01-16 13:05:02 +0100

    feat: added Scale widget

--- client/main.py
+++ client/main.py
@@ -1,77 +1,39 @@
 #==== Imports ====#
 from tkinter import *
 import json
+from ..server.summarization import extractive_summarization, abstractive_summarization
 
 
 #==== Function Definitions ====#
-# # Save Password
-# def save():
-#     site = website.get()
-#     mail = email.get()
-#     pass_ = password.get()
-#     credentials = {
-#         site: {
-#             "email": mail,
-#             "password": pass_
-#         }
-#     }
+# Summarization
+def summarizer():
+    input = text.get()
 
-#     if len(site) == 0 or len(pass_) == 0:
-#         messagebox.showwarning(title = "Empty Fields",
-#                                message = "Please don't leave any field empty")
-#     else:
-#         try:
-#             with open(FILE, "r") as file:
-#                 data = json.load(file)
-#         except FileNotFoundError:
-#             with open(FILE, "w") as file:
-#                 json.dump(credentials, file, indent = 4)
-#         else:
-#             data.update(credentials)
+    if len(site) == 0 or len(pass_) == 0:
+        messagebox.showwarning(title = "Empty Fields",
+                               message = "Please don't leave any field empty")
+    else:
+        try:
+            with open(FILE, "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open(FILE, "w") as file:
+                json.dump(credentials, file, indent = 4)
+        else:
+            data.update(credentials)
 
-#             with open(FILE, "w") as file:
-#                 json.dump(data, file, indent=4)
-#         finally:
-#             website.delete(0, END)
-#             password.delete(0, END)
-
-# # Password Generator
-# def gen_passcode():
-#     password_letters = [choice(letters) for _ in range(randint(8, 10))]
-#     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
-#     password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
-
-#     password_list = password_letters + password_symbols + password_numbers
-#     shuffle(password_list)
-
-#     passcode = "".join(password_list)
-#     password.insert(0, passcode)
-#     pyperclip.copy(passcode)
-
-# # Find Password
-# def find_password():
-#     try:
-#         with open(FILE, "r") as file:
-#             data = json.load(file)
-
-#             site = website.get()
-#             mail = data[site]["email"]
-#             pass_ = data[site]["password"]
-#     except FileNotFoundError:
-#         messagebox.showwarning(title = "Missing File",
-#                                message = "No password file found")
-#     except KeyError:
-#         messagebox.showwarning(title = "Missing Data",
-#                                message = f"No details for {site} exists")
-#     else:
-#         messagebox.showinfo(title = site, message = f"Email/Username: {mail}\nPassword: {pass_}")
-
+            with open(FILE, "w") as file:
+                json.dump(data, file, indent=4)
+        finally:
+            website.delete(0, END)
+            password.delete(0, END)
 
 
 #==== Declarations ====#
 window = Tk()
-text = Text(height = 17, width = 155)
-summarize = Button(text = "Summarize") #, command = gen_passcode)
+text = Text(height = 17, width = 130)
+scale = Spinbox(from_ = 0, to = 100, width = 7)  #, command = scale_widget)
+summarize = Button(text = "Summarize", command = summarizer)
 output = Label(bg = "white", height = 12, width = 135, font = ("Times New Roman", 12))
 
 
@@ -82,6 +44,8 @@ window.config(padx = 50, pady = 50)
 
 text.grid(column=0, row=0, columnspan=5, rowspan=5, padx=10, pady=10)
 text.focus()
+
+scale.grid(column = 5, row = 1)
 
 summarize.config(padx=20)
 summarize.grid(column = 2, row = 5, pady=10)
